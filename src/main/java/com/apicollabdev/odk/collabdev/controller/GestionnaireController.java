4079ea83e20076ccacf8d385430549746ff4a559
Mamoutou02 <sangaremoutou.360@.com>
2025-07-10 11:33:59 +0000

    MAJ

--- src/main/java/com/apicollabdev/odk/collabdev/controller/GestionnaireController.java
+++ src/main/java/com/apicollabdev/odk/collabdev/controller/GestionnaireController.java
@@ -1,48 +1,37 @@
 package com.apicollabdev.odk.collabdev.controller;
 
+import com.apicollabdev.odk.collabdev.dto.GestionnaireDTO;
 import com.apicollabdev.odk.collabdev.entity.Gestionnaire;
 import com.apicollabdev.odk.collabdev.service.GestionnaireService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-@RequestMapping("/api/gestionnaires")
 @RestController
+@RequestMapping("/api/gestionnaires")
 public class GestionnaireController {
 
-    private GestionnaireService gestionnaireService;
+    private final GestionnaireService gestionnaireService;
 
-    @PostMapping
-    public ResponseEntity<Gestionnaire> createGestionnaire(@RequestBody Gestionnaire gestionnaire) {
-        Gestionnaire saved = gestionnaireService.createGestionnaire(gestionnaire);
-        return ResponseEntity.ok(saved);
+    public GestionnaireController(GestionnaireService gestionnaireService) {
+        this.gestionnaireService = gestionnaireService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Gestionnaire>> getAllGestionnaires() {
-        return ResponseEntity.ok(gestionnaireService.getAllGestionnaires());
+    @PostMapping("/valider-contribution")
+    public void validerContribution(@RequestBody GestionnaireDTO gestionnaireDTO) {
+        gestionnaireService.validerContribution(gestionnaireDTO);
     }
 
+    @PostMapping("/valider-demande")
+    public void validerDemande(@RequestBody GestionnaireDTO gestionnaireDTO) {
+        gestionnaireService.validerDemande(gestionnaireDTO);
+    }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Gestionnaire> getGestionnaireById(@PathVariable Long id) {
-        return ResponseEntity.ok(gestionnaireService.getGestionnaireById(id));
+    public Gestionnaire getGestionnaireById(@PathVariable Long id) {
+        return gestionnaireService.getGestionnaireById(id);
     }
 
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Gestionnaire> updateGestionnaire(
-            @PathVariable Long id,
-            @RequestBody Gestionnaire updatedGestionnaire) {
-
-        Gestionnaire updated = gestionnaireService.updateGestionnaire(id, updatedGestionnaire);
-        return ResponseEntity.ok(updated);
-    }
-
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteGestionnaire(@PathVariable Long id) {
-        gestionnaireService.deleteGestionnaire(id);
-        return ResponseEntity.noContent().build();
+    @PutMapping
+    public Gestionnaire updateGestionnaire(@RequestBody Gestionnaire gestionnaire) {
+        return gestionnaireService.updateGestionnaire(gestionnaire);
     }
 }
--- src/main/java/com/apicollabdev/odk/collabdev/entity/Contributeur.java
+++ src/main/java/com/apicollabdev/odk/collabdev/entity/Contributeur.java
@@ -20,7 +20,7 @@ public class Contributeur extends Utilisateur{
 
     private String nom;
     private String prenom;
-
+    private int coins = 0;
     @Enumerated(EnumType.STRING)
     private Profil profil; // DEVELOPPER, DESIGNER, etc
 
@@ -36,7 +36,7 @@ public class Contributeur extends Utilisateur{
 
 
     @OneToMany(mappedBy = "contributeur", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Coins> coins;
+    private List<Coins> coinsH;
 
     @OneToMany(mappedBy = "contributeur", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Badge> badges;
@@ -53,5 +53,101 @@ public class Contributeur extends Utilisateur{
     @OneToMany(mappedBy = "contributeur", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Recevoir> recevoirs;
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+
+    public List<Badge> getBadges() {
+        return badges;
+    }
+
+    public void setBadges(List<Badge> badges) {
+        this.badges = badges;
+    }
+
+    public List<DemandeParticipation> getDemandeParticipations() {
+        return demandeParticipations;
+    }
+
+    public void setDemandeParticipations(List<DemandeParticipation> demandeParticipations) {
+        this.demandeParticipations = demandeParticipations;
+    }
+
+    public List<Demande> getDemandes() {
+        return demandes;
+    }
+
+    public void setDemandes(List<Demande> demandes) {
+        this.demandes = demandes;
+    }
+
+    public List<DebloqueProjet> getDebloqueProjets() {
+        return debloqueProjets;
+    }
+
+    public void setDebloqueProjets(List<DebloqueProjet> debloqueProjets) {
+        this.debloqueProjets = debloqueProjets;
+    }
+
+    public List<Recevoir> getRecevoirs() {
+        return recevoirs;
+    }
+
+    public void setRecevoirs(List<Recevoir> recevoirs) {
+        this.recevoirs = recevoirs;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    public Profil getProfil() {
+        return profil;
+    }
+
+    public void setProfil(Profil profil) {
+        this.profil = profil;
+    }
+
+    public Niveau getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(Niveau niveau) {
+        this.niveau = niveau;
+    }
+
+    public List<IdeeProjet> getIdeeProjets() {
+        return ideeProjets;
+    }
+
+    public void setIdeeProjets(List<IdeeProjet> ideeProjets) {
+        this.ideeProjets = ideeProjets;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public List<Coins> getCoinsH() {
+        return coinsH;
+    }
+
+    public void setCoinsH(List<Coins> coinsH) {
+        this.coinsH = coinsH;
+    }
 }
 
